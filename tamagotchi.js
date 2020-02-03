e374d31a7d2ff78174a137f18cd3678e5c4bec6e
Reinoptland <reinoptland@gmail.com>
2020-02-03 10:32:54 +0100

    Initialize data in constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,7 +1,24 @@
 // tamagotchi.js
 class Tamagotchi {
-  constructor() {
+  constructor(name) {
+    this.name = name;
+    this.gender = "m";
+    this.family = "Zork";
+    this.birthDate = new Date();
+
+    this.hunger = 0;
+    this.happiness = 0;
+    this.discipline = 0;
+    this.age = 0;
+    this.numberOfPoops = 0;
+    this.weightGrams = 1000;
+
+    this.isIll = false;
     this.isHatched = false;
+    this.isPottyTrained = false;
+
+    this.lifeCycles = ["Baby", "Teen", "Adult", "Senior"];
+    this.currentLifeCycle = 0;
   }
   hatch() {
     console.log("Our egg is hatching! The miracle of life!");
@@ -9,7 +26,7 @@ class Tamagotchi {
   }
 }
 
-const pet = new Tamagotchi();
+const pet = new Tamagotchi("Stinky");
 console.log(pet);
 pet.hatch();
 console.log(pet);
